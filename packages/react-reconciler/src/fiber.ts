f9602d0936fcd2d021f374bef943752ca39e9a37
TRoYals <aozora00321@gmail.com>
2024-07-23 10:02:01 +0800

    [DOC]FiberNode的alternate属性和flags属性的说明

--- packages/react-reconciler/src/fiber.ts
+++ packages/react-reconciler/src/fiber.ts
@@ -6,22 +6,24 @@ export class FiberNode {
 	memoizedProps: Props | null;
 	key: Key;
 	stateNode: any;
-	type: any;
+	type: any;//stateNode是FiberNode类的一个属性，用于存储与该FiberNode相关联的具体状态对象。这个属性的具体内容取决于FiberNode的类型（即tag的值）
 	ref: Ref;
 	tag: WorkTag;//tag是FiberNode类的一个属性，用来标识这个FiberNode的类型
-	flags: Flags;
+	flags: Flags;//标记的是浏览器宿主API的一系列操作
 	subtreeFlags: Flags;
 	deletions: FiberNode[] | null;
 
-	return: FiberNode | null;
-	sibling: FiberNode | null;
-	child: FiberNode | null;
-	index: number;
+	//构成树状结构
+	return: FiberNode | null; //指向父fiberNode
+	sibling: FiberNode | null; //指向右边的兄弟fiberNode
+	child: FiberNode | null; //指向子fiberNode
+	index: number; //列表中的index等
 
+	//作为工作单元
 	updateQueue: unknown;
 	memoizedState: any;
 
-	alternate: FiberNode | null;
+	alternate: FiberNode | null; //用于FiberNode中的 current和workingInProgress树（双缓存技术）
 
 	lanes: Lanes;
 
